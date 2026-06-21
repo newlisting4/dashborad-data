1d87a44023cc36e535c693c94cb9c2853a024c7e
abdurrahim220 <z.abdurrahim5@gmail.com>
2026-06-21 15:31:09 +0600

    updated

--- components/common/Loader.js
+++ components/common/Loader.js
@@ -1,20 +1,34 @@
 import React from "react";
-import { ThreeCircles } from "react-loader-spinner";
 
 function Loader({ isLoading, children }) {
   const loader = (
-    <ThreeCircles
-      height="100"
-      width="100"
-      color="#7A80B4"
-      wrapperStyle={{}}
-      wrapperClass=""
-      visible={true}
-      ariaLabel="three-circles-rotating"
-      outerCircleColor=""
-      innerCircleColor=""
-      middleCircleColor=""
-    />
+    <div className="relative w-24 h-24 flex items-center justify-center">
+      {/* Outer circle */}
+      <div 
+        className="absolute w-24 h-24 border-4 rounded-full animate-spin" 
+        style={{
+          borderColor: "#7A80B4 transparent transparent transparent",
+          animationDuration: "1.2s"
+        }}
+      ></div>
+      {/* Middle circle */}
+      <div 
+        className="absolute w-16 h-16 border-4 rounded-full animate-spin" 
+        style={{
+          borderColor: "transparent transparent #7A80B4 transparent",
+          animationDuration: "1.5s",
+          animationDirection: "reverse"
+        }}
+      ></div>
+      {/* Inner circle */}
+      <div 
+        className="absolute w-8 h-8 border-4 rounded-full animate-spin" 
+        style={{
+          borderColor: "#7A80B4 transparent transparent transparent",
+          animationDuration: "0.8s"
+        }}
+      ></div>
+    </div>
   );
 
   return (
@@ -31,3 +45,4 @@ function Loader({ isLoading, children }) {
 }
 
 export default Loader;
+
--- components/common/Spinner.js
+++ components/common/Spinner.js
@@ -1,22 +1,30 @@
-import { Oval } from "react-loader-spinner";
+import React from "react";
 
 function Spinner() {
   return (
-    <>
-      <Oval
-        height={33}
-        width={33}
-        color="#4fa94d"
-        wrapperStyle={{}}
-        wrapperClass=""
-        visible={true}
-        ariaLabel="oval-loading"
-        secondaryColor="#4fa94d"
-        strokeWidth={5}
-        strokeWidthSecondary={2}
+    <svg
+      className="animate-spin h-8 w-8"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      style={{ color: "#4fa94d" }}
+    >
+      <circle
+        className="opacity-25"
+        cx="12"
+        cy="12"
+        r="10"
+        stroke="currentColor"
+        strokeWidth="4"
       />
-    </>
+      <path
+        className="opacity-75"
+        fill="currentColor"
+        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+      />
+    </svg>
   );
 }
 
 export default Spinner;
+
